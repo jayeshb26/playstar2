e287c24eaffaf3bf833aab765f611dcb85f5c9fb
jayeshb26 <jayeshb26@gmail.com>
2024-05-25 16:09:39 +0530

    userdata

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -44,7 +44,7 @@ const UserController = {
       //   secure: true,
       //   sameSite: "None",
       // }).status(200).json(userData);
-      res.status(200).json({ userData });
+      res.status(200).json(userData );
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
